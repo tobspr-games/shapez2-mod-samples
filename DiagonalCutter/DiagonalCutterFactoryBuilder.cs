0f67ed87efe8233e7e6531726639fc5b182c0c6d
Lorenzo <lorenzo.kaufmann@tobspr.io>
2025-09-09 14:40:23 -0300

    Update Diagonal Cutter mod with latest Shapez Shifter fluent API

---
+++ DiagonalCutter/DiagonalCutterFactoryBuilder.cs
@@ -0,0 +1,21 @@
+﻿using Core.Factory;
+using ShapezShifter;
+using ShapezShifter.Fluent.Atomic;
+
+internal class DiagonalCutterFactoryBuilder
+    : IFactoryBuilder<DiagonalCutterSimulation, DiagonalCutterSimulationState, DiagonalCutterConfiguration>
+{
+    public IFactory<DiagonalCutterSimulationState, DiagonalCutterSimulation> BuildFactory(
+        SimulationSystemsDependencies dependencies,
+        out DiagonalCutterConfiguration config)
+    {
+        config = new DiagonalCutterConfiguration(
+            BuffableBeltSpeed.DiscreteSpeed.OneSecondPerTile,
+            BuffableBeltDelay.DiscreteDuration.OnePointFiveSeconds,
+            new ResearchSpeedId("CutterSpeed"));
+
+        var diagonalCut = new ShapeOperationDiagonalCut(dependencies.Mode.MaxShapeLayers);
+
+        return new DiagonalCutterSimulationFactory(config, dependencies.ShapeRegistry, diagonalCut);
+    }
+}
--- DiagonalCutter/DiagonalCutterMod.cs
+++ DiagonalCutter/DiagonalCutterMod.cs
@@ -1,46 +1,91 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.IO;
 using Core.Localization;
 using JetBrains.Annotations;
 using ShapezShifter;
+using ShapezShifter.Fluent;
+using ShapezShifter.Fluent.Atomic;
+using ShapezShifter.Fluent.Toolbar;
+using UnityEngine;
 using ILogger = Core.Logging.ILogger;
+using OpResult = ShapeOperationDiagonalCut;
+using Operation = ShapeDiagonalCutResult;
+using Renderer = DiagonalCutterSimulationRenderer;
+using Simulation = DiagonalCutterSimulation;
+using RendererData = IDiagonalCutterDrawData;
 
 [UsedImplicitly]
 public class DiagonalCuttersMod : IMod
 {
-    private readonly DiagonalCutterBuildingsExtender DiagonalCutterBuildingsExtender;
-    private readonly List<ExtenderHandle> ExtenderHandles = new();
+    private readonly IDisposable Extender;
 
     public DiagonalCuttersMod(ILogger logger)
     {
-        DiagonalCutterBuildingsExtender = new DiagonalCutterBuildingsExtender(logger);
-        ExtenderHandle handle = ShapezExtensions.AddExtender(DiagonalCutterBuildingsExtender);
-        ExtenderHandles.Add(handle);
+        BuildingDefinitionGroupId groupId = new("DiagonalCutterGroup");
+        BuildingDefinitionId definitionId = new("DiagonalCutter");
 
-        var database = (LocalizationDatabase)Globals.Localization.DatabaseProvider.CurrentDatabase;
+        string titleId = "building-variant.cutter-diagonal.title";
+        string titleDescription = "building-variant.cutter-diagonal.description";
+        string title = "Diagonal Destroyer";
+        string description = "<gl>Destroys</gl> the <gl>Even Parts</gl> of any shape.";
 
-        var newKeys = new Dictionary<string, string>
+        TranslationBatch.Begin()
+           .AddEntry(titleId, TranslationEntry.WithDefault(title))
+           .AddEntry(titleDescription, TranslationEntry.WithDefault(description))
+           .Flush();
+
+        IBuildingGroupBuilder diagonalCutterGroup = BuildingGroup.Create(groupId)
+           .WithTitle(title.T())
+           .WithDescription(description.T())
+           .WithIcon(LoadIcon())
+           .AsNonTransportableBuilding()
+           .WithPreferredPlacement(DefaultPreferredPlacementMode.LinePerpendicular)
+           .WithDefaultThroughputDisplayHelper();
+
+        IBuildingConnectorData connectorData = Connectors.SingleTile()
+           .AddShapeInput(ShapeConnectorConfig.DefaultInput())
+           .AddShapeOutput(ShapeConnectorConfig.DefaultOutput())
+           .Build();
+
+        IBuildingBuilder diagonalCutterBuilder = Building.Create(definitionId)
+           .WithConnectorData(connectorData)
+           .DynamicallyRendering<Renderer, Simulation, RendererData>(new DiagonalCutterDrawData())
+           .WithCopiedStaticDrawData(new BuildingDefinitionId("CutterHalfInternalVariant"))
+           .WithPrediction(new AtomicPredictor<Operation>(new OpResult(4), ResultingShape))
+           .WithoutSound()
+           .WithoutSimulationConfiguration()
+           .WithEfficiencyData(new BuildingEfficiencyData(2.0f, 1));
+
+        Extender = AtomicBuildings.Extend()
+           .AllScenarios()
+           .WithBuilding(diagonalCutterBuilder, diagonalCutterGroup)
+           .WithDefaultPlacement()
+           .InToolbar(ToolbarElementLocator.Root().ChildAt(0).ChildAt(3).ChildAt(^1).InsertAfter())
+           .WithSimulation(new DiagonalCutterFactoryBuilder())
+           .WithAtomicShapeProcessingModules(BuiltinResearchSpeed.CutterSpeed, 2.0f)
+           .Build();
+
+        return;
+
+        ShapeCollapseResult ResultingShape(Operation result)
         {
-            { "building-variant.cutter-diagonal.title", "Diagonal Destroyer" },
-            {
-                "building-variant.cutter-diagonal.description",
-                "<gl>Destroys</gl> the <gl>Even Parts</gl> of any shape."
-            }
-        };
-        var translationParser = new TranslationParser().Parse(newKeys);
-        foreach (var parsedTranslation in translationParser)
-        {
-            database.Entries.Add(parsedTranslation.Key, parsedTranslation.Value);
+            return result.RightSide;
         }
     }
 
     public void Dispose()
     {
-        foreach (ExtenderHandle handle in ExtenderHandles)
-        {
-            ShapezExtensions.RemoveExtender(handle);
-        }
+        Extender.Dispose();
+    }
 
-        DiagonalCutterBuildingsExtender.Dispose();
-        ExtenderHandles.Clear();
+    private static Sprite LoadIcon()
+    {
+        string basePath = Path.GetDirectoryName(typeof(DiagonalCuttersMod).Assembly.Location);
+        string resourcesPath = Path.Combine(basePath, "Resources", "DiagonalCutter_Icon.png");
+        byte[] data = File.ReadAllBytes(resourcesPath);
+
+        var texture = new Texture2D(2, 2);
+        texture.LoadImage(data);
+        return Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), new Vector2(0.5f, 0.5f));
     }
 }
